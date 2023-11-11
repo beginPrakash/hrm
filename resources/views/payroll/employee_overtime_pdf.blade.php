3702730c1a1dee0748740e2afccc78161e554119
hetal <hetal.spotarrow@gmail.com>
2023-11-12 00:18:56 +0530

    overtime pdf changes

--- resources/views/payroll/employee_overtime_pdf.blade.php
+++ resources/views/payroll/employee_overtime_pdf.blade.php
@@ -44,57 +44,154 @@
 </head>
 <body>
     <div style="display: block; margin:0 auto;">
-    <table class="custom-table" style="margin-bottom: 30px;max-width:400px;">
+    @if(isset($emp_branch_data) && count($emp_branch_data) > 0)
+        @foreach($emp_branch_data as $bkey => $bval)
+            <table style="margin-bottom: 30px;max-width:400px;">
+                <tbody>
+                    @if($bkey == 0)
+                    <tr>
+                        <th colspan="2" class="text-center">
+                            <a href="{{ url('/') }}">
+                                <img src="{{ asset('assets/img/logo1.png') }}" style="width: 140px;">
+                            </a>
+                        </th>
+                    </tr>
+                    @endif
+                    <tr>
+                        <th width="80px" style="vertical-align:top">                        
+                            <p class="text-left mb-0">
+                                <b>Branch: </b>{{$bval->branch_name}}
+                            </p>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+             
+            <table class="custom-table" style="margin-bottom: 30px;max-width:400px;">
                 <thead style="background-color: #F5F5F5">
                     <tr>
-                    <th>Sr.No</th>
-                    <th>Employee ID</th>
-                    <th>Employee Name</th>
-                    <th>Overtime Hours</th>
-                    <th>Overtime Amount</th>
-                    <th>Bonus</th>
-                    <th>Total Earning</th>
+                        <th>S.no</th>
+                        <th>Emp.Id</th>
+                        <th>Name</th>
+                        <th>Salary</th>
+                        <th>Food Allowance</th>
+                        <th>Addition of all other Allowances</th>
+                        <th>Bonus</th>
+                        <th>Total Earning</th>
                     </tr>
                 </thead>
                 <tbody>
                             
-                    @if(isset($emp_overtime_data) && count($emp_overtime_data) > 0)
-                        @php $i = 1; @endphp
-                        
-                        @foreach($emp_overtime_data as $key => $val)
+                @if(isset($emp_salary_data) && count($emp_salary_data) > 0)
+                    @php $i = 1; @endphp
+                    
+                    @foreach($emp_salary_data as $key => $val)
+                    @php $total_allowence = total_allowence_withput_food($val->id); @endphp
+                        @if($val->branch_name == $bval->branch_name)
                         @php $bonus = calculateBonusByMonth($val->employee_id,$month,$year);
                                         $total_earning = $val->overtime_amount + $bonus;
                                         @endphp
-                                <tr>
-                                    <td>
-                                        <h3 class="mb-0">{{$i}}</h3>
-                                    </td>
-                                    <td>
-                                        <p>{{$val->employee_id}}</p>
-                                    </td>
-                                    <td>
-                                        <p>{{$val->name}}</p>
-                                    </td>
-                                    <td>
-                                        <p>{{ number_format($val->total_overtime_hours,1) ?? 0}}</p> 
-                                    </td>
-                                    <td>
-                                        <p>{{ number_format($val->overtime_amount,2) }}</p>
-                                    </td>
-                                    <td>
-                                        <p>{{$bonus}}</p>
-                                    </td>
-                                    <td>
-                                        <p>{{ number_format($total_earning, 2) }}</p>
-                                    </td>
-                                </tr>
-                                @php $i++; @endphp    
-                            
+                            <tr>
+                                <td>
+                                    <h3 class="mb-0">{{$i}}</h3>
+                                </td>
+                                <td>
+                                    <p>{{$val->employee_id}}</p>
+                                </td>
+                                <td>
+                                    <p>{{$val->name}}</p>
+                                </td>
+                                <td>
+                                    <p>{{ number_format($val->overtime_amount, 2) }}</p> 
+                                </td>
+                                <td>
+                                    <p>{{$val->food_allowence ?? 0}}</p>
+                                </td>
+                                <td>
+                                    <p>{{$total_allowence ?? 0}}</p>
+                                </td>
+                                <td>
+                                    <p>{{$bonus}}</p>
+                                </td>
+                                <td>
+                                    <p>{{ number_format($val->total_earning, 2) }}</p>
+                                </td>
+                            </tr>
+                            @php $i++; @endphp    
+                         @endif  
+                        
+                    @endforeach
+                @endif
+               
+                        </tbody>
+            </table>
+        @endforeach  
+        
+        @php $currentMonth = date('F', mktime(0, 0, 0, $month, 10)); @endphp
+        <div style="font-size: 20px; margin-bottom:10px; font-weight: bold;">Summary of {{$currentMonth}} {{$year}}</div>
+
+        <table class="table table-bordered custom-table">
+            <tbody> 
+                <tr>
+                    <td>Branch</td>
+                    @if(isset($emp_company_data) && count($emp_company_data) > 0)
+                        @foreach($emp_company_data as $ckey => $cval)
+                            <td colspan="2">{{$cval->company_name}}</td>
                         @endforeach
                     @endif
-               
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    @if(isset($emp_company_data) && count($emp_company_data) > 0)
+                        @foreach($emp_company_data as $ckey => $cval)
+                            <td>Cash</td>
+                            <td>Bank</td>
+                        @endforeach
+                    @endif
+                    <td>Total</td>
+                </tr>
+                    
+                @if(isset($emp_branch_data) && count($emp_branch_data) > 0)
+                    @foreach($emp_branch_data as $bkey => $bval)
+                        <tr>
+                            <td>{{$bval->branch_name}}</td>
+                            @if(isset($emp_company_data) && count($emp_company_data) > 0)
+                                @foreach($emp_company_data as $ckey => $cval)
+                                    <td>{{ number_format(calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'cash'), 2) }}</td>
+                                    <td>{{calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'bank')}}</td>
+                                @endforeach
+                            @endif
+                            <td>{{ number_format(calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'','total'), 2) }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+                <tr>
+                    <td>Total</td>
+                    @if(isset($emp_company_data) && count($emp_company_data) > 0)
+                        @foreach($emp_company_data as $ckey => $cval)
+                            <td>{{ number_format(calcualte_ot_total_by_month_company($month,$year,$cval->company_id,'cash'), 2) }}</td>
+                            <td>{{ number_format(calcualte_ot_total_by_month_company($month,$year,$cval->company_id,'bank'), 2) }}</td>
+                        @endforeach
+                    @endif
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="margin-top: 20px;">
+                <tbody>
+                <tr>
+                    <td><b>HR Manager Sign.</b></td>
+                    <td><b>Sr. Accountant Sign.</b></td>
+                    <td><b>Finanace Manager Sign.</b></td>
+                    <td><b>General Manager Sign.</b></td>
+                    <td><b>CEO Sign.</b></td>
+                </tr>
                 </tbody>
-            </table>
+        </table>
+
+
+    @endif
     </div>
 </body>
 </html>
--- storage/framework/views/bfc02ba4a12b04ec890380071d41c2af3b2718ce.php
+++ storage/framework/views/bfc02ba4a12b04ec890380071d41c2af3b2718ce.php
@@ -44,58 +44,156 @@
 </head>
 <body>
     <div style="display: block; margin:0 auto;">
-    <table class="custom-table" style="margin-bottom: 30px;max-width:400px;">
+    <?php if(isset($emp_branch_data) && count($emp_branch_data) > 0): ?>
+        <?php $__currentLoopData = $emp_branch_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $bkey => $bval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <table style="margin-bottom: 30px;max-width:400px;">
+                <tbody>
+                    <?php if($bkey == 0): ?>
+                    <tr>
+                        <th colspan="2" class="text-center">
+                            <a href="<?php echo e(url('/')); ?>">
+                                <img src="<?php echo e(asset('assets/img/logo1.png')); ?>" style="width: 140px;">
+                            </a>
+                        </th>
+                    </tr>
+                    <?php endif; ?>
+                    <tr>
+                        <th width="80px" style="vertical-align:top">                        
+                            <p class="text-left mb-0">
+                                <b>Branch: </b><?php echo e($bval->branch_name); ?>
+
+                            </p>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+             
+            <table class="custom-table" style="margin-bottom: 30px;max-width:400px;">
                 <thead style="background-color: #F5F5F5">
                     <tr>
-                    <th>Sr.No</th>
-                    <th>Employee ID</th>
-                    <th>Employee Name</th>
-                    <th>Overtime Hours</th>
-                    <th>Overtime Amount</th>
-                    <th>Bonus</th>
-                    <th>Total Earning</th>
+                        <th>S.no</th>
+                        <th>Emp.Id</th>
+                        <th>Name</th>
+                        <th>Salary</th>
+                        <th>Food Allowance</th>
+                        <th>Addition of all other Allowances</th>
+                        <th>Bonus</th>
+                        <th>Total Earning</th>
                     </tr>
                 </thead>
                 <tbody>
                             
-                    <?php if(isset($emp_overtime_data) && count($emp_overtime_data) > 0): ?>
-                        <?php $i = 1; ?>
-                        
-                        <?php $__currentLoopData = $emp_overtime_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $val): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php if(isset($emp_salary_data) && count($emp_salary_data) > 0): ?>
+                    <?php $i = 1; ?>
+                    
+                    <?php $__currentLoopData = $emp_salary_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $val): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <?php $total_allowence = total_allowence_withput_food($val->id); ?>
+                        <?php if($val->branch_name == $bval->branch_name): ?>
                         <?php $bonus = calculateBonusByMonth($val->employee_id,$month,$year);
                                         $total_earning = $val->overtime_amount + $bonus;
                                         ?>
-                                <tr>
-                                    <td>
-                                        <h3 class="mb-0"><?php echo e($i); ?></h3>
-                                    </td>
-                                    <td>
-                                        <p><?php echo e($val->employee_id); ?></p>
-                                    </td>
-                                    <td>
-                                        <p><?php echo e($val->name); ?></p>
-                                    </td>
-                                    <td>
-                                        <p><?php echo e(number_format($val->total_overtime_hours,1) ?? 0); ?></p> 
-                                    </td>
-                                    <td>
-                                        <p><?php echo e(number_format($val->overtime_amount,2)); ?></p>
-                                    </td>
-                                    <td>
-                                        <p><?php echo e($bonus); ?></p>
-                                    </td>
-                                    <td>
-                                        <p><?php echo e(number_format($total_earning, 2)); ?></p>
-                                    </td>
-                                </tr>
-                                <?php $i++; ?>    
-                            
+                            <tr>
+                                <td>
+                                    <h3 class="mb-0"><?php echo e($i); ?></h3>
+                                </td>
+                                <td>
+                                    <p><?php echo e($val->employee_id); ?></p>
+                                </td>
+                                <td>
+                                    <p><?php echo e($val->name); ?></p>
+                                </td>
+                                <td>
+                                    <p><?php echo e(number_format($val->overtime_amount, 2)); ?></p> 
+                                </td>
+                                <td>
+                                    <p><?php echo e($val->food_allowence ?? 0); ?></p>
+                                </td>
+                                <td>
+                                    <p><?php echo e($total_allowence ?? 0); ?></p>
+                                </td>
+                                <td>
+                                    <p><?php echo e($bonus); ?></p>
+                                </td>
+                                <td>
+                                    <p><?php echo e(number_format($val->total_earning, 2)); ?></p>
+                                </td>
+                            </tr>
+                            <?php $i++; ?>    
+                         <?php endif; ?>  
+                        
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                <?php endif; ?>
+               
+                        </tbody>
+            </table>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>  
+        
+        <?php $currentMonth = date('F', mktime(0, 0, 0, $month, 10)); ?>
+        <div style="font-size: 20px; margin-bottom:10px; font-weight: bold;">Summary of <?php echo e($currentMonth); ?> <?php echo e($year); ?></div>
+
+        <table class="table table-bordered custom-table">
+            <tbody> 
+                <tr>
+                    <td>Branch</td>
+                    <?php if(isset($emp_company_data) && count($emp_company_data) > 0): ?>
+                        <?php $__currentLoopData = $emp_company_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ckey => $cval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <td colspan="2"><?php echo e($cval->company_name); ?></td>
                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                     <?php endif; ?>
-               
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <?php if(isset($emp_company_data) && count($emp_company_data) > 0): ?>
+                        <?php $__currentLoopData = $emp_company_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ckey => $cval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <td>Cash</td>
+                            <td>Bank</td>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    <?php endif; ?>
+                    <td>Total</td>
+                </tr>
+                    
+                <?php if(isset($emp_branch_data) && count($emp_branch_data) > 0): ?>
+                    <?php $__currentLoopData = $emp_branch_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $bkey => $bval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td><?php echo e($bval->branch_name); ?></td>
+                            <?php if(isset($emp_company_data) && count($emp_company_data) > 0): ?>
+                                <?php $__currentLoopData = $emp_company_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ckey => $cval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <td><?php echo e(number_format(calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'cash'), 2)); ?></td>
+                                    <td><?php echo e(calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'bank')); ?></td>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            <?php endif; ?>
+                            <td><?php echo e(number_format(calcualte_ot_total_earning_by_month_company($month,$year,$cval->company_id,$bval->id,'','total'), 2)); ?></td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                <?php endif; ?>
+                <tr>
+                    <td>Total</td>
+                    <?php if(isset($emp_company_data) && count($emp_company_data) > 0): ?>
+                        <?php $__currentLoopData = $emp_company_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ckey => $cval): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <td><?php echo e(number_format(calcualte_ot_total_by_month_company($month,$year,$cval->company_id,'cash'), 2)); ?></td>
+                            <td><?php echo e(number_format(calcualte_ot_total_by_month_company($month,$year,$cval->company_id,'bank'), 2)); ?></td>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    <?php endif; ?>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="margin-top: 20px;">
+                <tbody>
+                <tr>
+                    <td><b>HR Manager Sign.</b></td>
+                    <td><b>Sr. Accountant Sign.</b></td>
+                    <td><b>Finanace Manager Sign.</b></td>
+                    <td><b>General Manager Sign.</b></td>
+                    <td><b>CEO Sign.</b></td>
+                </tr>
                 </tbody>
-            </table>
+        </table>
+
+
+    <?php endif; ?>
     </div>
 </body>
 </html>
-<?php //exit; ?><?php /**PATH C:\wamp64_new\www\hrm\resources\views/payroll/employee_overtime_pdf.blade.php ENDPATH**/ ?>
+<?php exit; ?><?php /**PATH C:\wamp64_new\www\hrm\resources\views/payroll/employee_overtime_pdf.blade.php ENDPATH**/ ?>
